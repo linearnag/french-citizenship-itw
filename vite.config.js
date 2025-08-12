e2bb327246a5c14af68aa928845da126a42bb851
lipsa <lipsanag0@gmail.com>
2025-08-12 16:47:37 -0400

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/french_naturalisation/', // Replace with your GitHub repository name
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
