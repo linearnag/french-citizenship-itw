d72b8569c482be83b0960fbd958541182a93d753
lipsa <lipsanag0@gmail.com>
2025-08-12 16:49:11 -0400

    Added deployment file and updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/french_naturalisation/', // Replace with your GitHub repository name
+  base: '/french-citizenship-itw/', // Replace with your GitHub repository name
   server: {
     port: 3000,
     open: true
